bec362a66367fc62dbd73e8614371169cbb1d115
mateotomeho <148917918+mateotomeho@users.noreply.github.com>
2026-08-27 16:34:37 -0400

    Add Vercel Web Analytics

--- court-signal/src/main.tsx
+++ court-signal/src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Analytics } from '@vercel/analytics/react'
 import 'leaflet/dist/leaflet.css'
 import './index.css'
 import App from './App.tsx'
@@ -8,5 +9,6 @@ import App from './App.tsx'
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
+    <Analytics />
   </StrictMode>,
 )
