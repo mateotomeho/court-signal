94de4130f4818fc7754fb69e8d5c2830182d12cb
mateotomeho <148917918+mateotomeho@users.noreply.github.com>
2026-08-27 10:54:22 -0400

    Add status popups and custom map markers

--- court-signal/src/components/CourtMap.tsx
+++ court-signal/src/components/CourtMap.tsx
@@ -1,5 +1,7 @@
-import { MapContainer, Marker, TileLayer } from 'react-leaflet'
+import L from 'leaflet'
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 import type { Court } from '../types/court'
+import { getCourtStatus } from '../utils/getCourtStatus'
 
 type CourtMapProps = {
   courtList: Court[]
@@ -9,6 +11,21 @@ type CourtMapProps = {
 
 const barrhavenCenter: [number, number] = [45.279, -75.764]
 
+// Function to create a custom Leaflet icon for the court markers based on the number of available courts
+function getCourtMarkerIcon(availableCourts: number) {
+  const status = getCourtStatus(availableCourts)
+  const statusClassName = status.label.toLowerCase().replaceAll(' ', '-')
+
+  return L.divIcon({
+    className: 'court-map-marker',
+    html: `<span class="court-map-marker__pin court-map-marker__pin--${statusClassName}"></span>`,
+    iconSize: [28, 28],
+    iconAnchor: [14, 28],
+    popupAnchor: [0, -28],
+  })
+}
+
+// Function to render the court map with markers for each court
 function CourtMap({ courtList, onSelectCourt }: CourtMapProps) {
   return (
     <section className="court-map" aria-labelledby="map-heading">
@@ -28,15 +45,28 @@ function CourtMap({ courtList, onSelectCourt }: CourtMapProps) {
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-        {courtList.map((court) => (
-          <Marker
-            key={court.id}
-            position={[court.latitude, court.longitude]}
-            eventHandlers={{
-              click: () => onSelectCourt(court.id),
-            }}
-          />
-        ))}
+        {courtList.map((court) => {
+          const status = getCourtStatus(court.availableCourts)
+
+          return (
+            <Marker
+              key={court.id}
+              position={[court.latitude, court.longitude]}
+              icon={getCourtMarkerIcon(court.availableCourts)}
+              eventHandlers={{
+                click: () => onSelectCourt(court.id),
+              }}
+            >
+              <Popup>
+                <strong>{court.name}</strong>
+                <br />
+                <span>
+                  {status.icon} {status.label}
+                </span>
+              </Popup>
+            </Marker>
+          )
+        })}
       </MapContainer>
     </section>
   )
